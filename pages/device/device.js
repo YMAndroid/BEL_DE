76d7d19902455f6c21fccad06ab6049105acf240
meng.yang_wh <meng.yang_wh@unistrong.com>
2021-07-23 11:19:32 +0800

    添加主从机设备类型区分；

--- pages/device/device.js
+++ pages/device/device.js
@@ -2,6 +2,9 @@ const app = getApp();
 const utils = require('../../utils/util');
 Page({
   data: {
+    selectDeviceTypeIndex: 0, //0:从设备 1:主设备
+    deviceTypeList:['从设备','主设备'],
+    selectDeviceTypeValue:'从设备',
     inputText: '',
     receiveText: '',
     name: '',
@@ -12,42 +15,41 @@ Page({
     selectServiceUUID: '',
     selectServiceUUIDIndex: 0,
     selectCharUUID: '',
-    selectServiceUUIDIndex: 0,
+    selectCharUUIDIndex: 0,
     isOpenNotify: false,
-    deviceDataList:[{
-      deviceName:'设备1',
-      voltage:2.4,
-      vurrent:5
-    },{
-      deviceName:'设备2',
-      voltage:2.4,
-      vurrent:5
-    },{
-      deviceName:'设备3',
-      voltage:2.4,
-      vurrent:5
-    },{
-      deviceName:'设备4',
-      voltage:2.4,
-      vurrent:5
-    },{
-      deviceName:'设备5',
-      voltage:2.4,
-      vurrent:5
-    },{
-      deviceName:'设备6',
-      voltage:2.4,
-      vurrent:5
-    },{
-      deviceName:'设备7',
-      voltage:2.4,
-      vurrent:'5'
-    },{
-      deviceName:'设备8',
-      voltage:2.4,
-      vurrent:5
+    deviceDataList: [{
+      deviceName: '设备1',
+      voltage: 2.4,
+      vurrent: 5
+    }, {
+      deviceName: '设备2',
+      voltage: 2.4,
+      vurrent: 5
+    }, {
+      deviceName: '设备3',
+      voltage: 2.4,
+      vurrent: 5
+    }, {
+      deviceName: '设备4',
+      voltage: 2.4,
+      vurrent: 5
+    }, {
+      deviceName: '设备5',
+      voltage: 2.4,
+      vurrent: 5
+    }, {
+      deviceName: '设备6',
+      voltage: 2.4,
+      vurrent: 5
+    }, {
+      deviceName: '设备7',
+      voltage: 2.4,
+      vurrent: '5'
+    }, {
+      deviceName: '设备8',
+      voltage: 2.4,
+      vurrent: 5
     }],
-    scrollHeight:0
   },
   bindInput: function (e) {
     this.setData({
@@ -56,25 +58,23 @@ Page({
     console.log(e.detail.value)
   },
   Send: function () {
-    if(!this.data.inputText){
+    if (!this.data.inputText) {
       wx.showToast({
-        icon:'none',
+        icon: 'none',
         title: '请输入设备名称',
         duration: 2000
       })
       return;
     }
-    if(!this.data.selectCharUUID || !this.data.selectServiceUUID){
+    if (!this.data.selectCharUUID || !this.data.selectServiceUUID) {
       wx.showToast({
-        icon:'none',
+        icon: 'none',
         title: '请先选择serviceUUID、charUUID',
         duration: 2000
       })
       return;
     }
-    if(!this.data.isOpenNotify){
-      this.openBleNotify();
-    }
+
     var that = this
     if (that.data.connected) {
       var buffer = new ArrayBuffer(that.data.inputText.length)
@@ -82,7 +82,7 @@ Page({
       for (var i = 0; i < that.data.inputText.length; i++) {
         dataView[i] = that.data.inputText.charCodeAt(i)
       }
-      console.log("数据buff: ",buffer);
+      console.log("数据buff: ", buffer);
       wx.writeBLECharacteristicValue({
         deviceId: that.data.connectedDeviceId,
         serviceId: that.data.selectServiceUUID,
@@ -91,14 +91,17 @@ Page({
         success: function (res) {
           console.log('写数据成功：' + JSON.stringify(res));
           wx.showToast({
-            icon:'none',
+            icon: 'none',
             title: '写数据成功',
             duration: 2000
           })
+          that.setData({
+            name:that.data.inputText
+          })
         },
-        fail:function(res){
+        fail: function (res) {
           wx.showToast({
-            icon:'none',
+            icon: 'none',
             title: '写数据失败',
             duration: 2000
           })
@@ -120,13 +123,6 @@ Page({
   },
   onLoad: function (options) {
     var that = this
-    let idArr = ['#head']
-    utils.calcuComponentHeight(that,idArr,function(res) {
-      that.setData({
-        scrollHeight: wx.getSystemInfoSync().windowHeight - res[0].height
-      })
-    })
-   
     console.log(options)
     that.setData({
       name: options.name,
@@ -138,16 +134,6 @@ Page({
         console.log(res.services)
         that.setData({
           services: res.services
-        })
-        wx.getBLEDeviceCharacteristics({
-          deviceId: options.connectedDeviceId,
-          serviceId: res.services[0].uuid,
-          success: function (res) {
-            console.log(res.characteristics)
-            that.setData({
-              characteristics: res.characteristics
-            })
-          }
         })
       }
     })
@@ -170,6 +156,7 @@ Page({
       selectServiceUUIDIndex: e.detail.value,
       selectServiceUUID: this.data.services[e.detail.value].uuid
     })
+    this.getBLEDeviceCahars();
   },
 
   getCharUUID: function (e) {
@@ -180,32 +167,60 @@ Page({
   },
 
   openBleNotify: function () {
+    if (this.data.isOpenNotify) {
+      wx.showToast({
+        title: '通知已打开!',
+        icon: 'none'
+      })
+      return;
+    }
+
+    if (!this.data.selectServiceUUID) {
+      wx.showToast({
+        title: '请选择服务UUID',
+        icon: 'none'
+      })
+      return;
+    }
+
+    if (!this.data.selectCharUUID) {
+      wx.showToast({
+        title: '请选择特征值UUID',
+        icon: 'none'
+      })
+      return;
+    }
+
     wx.notifyBLECharacteristicValueChange({
       state: true,
       deviceId: this.data.connectedDeviceId,
       serviceId: this.data.selectServiceUUID,
       characteristicId: this.data.selectCharUUID,
-      success: res=> {
+      success: res => {
         console.log('启用notify成功')
         this.setData({
-          isOpenNotify:true
+          isOpenNotify: true
+        })
+        wx.showToast({
+          title: '通知打开成功!',
+          icon: 'none'
         })
       },
-      fail:function(res){
-        console.log('启用notify失败: ',res);
+      fail: function (res) {
+        console.log('启用notify失败: ', res);
       }
     })
   },
 
-  onUnload: function(){
+  onUnload: function () {
     console.log("onUnload");
-    if(this.data.connected){
+    if (this.data.connected) {
       wx.closeBLEConnection({
         deviceId: this.data.connectedDeviceId,
-        success:res=>{
+        success: res => {
           console.log("断开设备连接成功")
         },
-        fail:res=>{
+        fail: res => {
           console.log("断开连接的设备失败!");
           console.log(res);
         }
@@ -213,12 +228,32 @@ Page({
     }
   },
 
-  charUUIDtap:function(){
-    if(!this.data.selectServiceUUID){
+  charUUIDtap: function () {
+    if (!this.data.selectServiceUUID) {
       wx.showToast({
         title: '请先选择服务UUID',
-        icon:'none'
+        icon: 'none'
       })
     }
+  },
+
+  getBLEDeviceCahars: function () {
+    wx.getBLEDeviceCharacteristics({
+      deviceId: options.connectedDeviceId,
+      serviceId: this.data.selectServiceUUID,
+      success: function (res) {
+        console.log(res.characteristics)
+        that.setData({
+          characteristics: res.characteristics
+        })
+      }
+    })
+  },
+
+  getDeviceType: function(e){
+    this.setData({
+      selectDeviceTypeIndex:e.detail.value,
+      selectDeviceTypeValue:this.data.deviceTypeList[e.detail.value]
+    })
   }
 })
